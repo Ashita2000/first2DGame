bf39a7a51d046a66f0c500e6a2bae94aceabc3e2
agarg5 <Anchal_Garg@intuit.com>
2020-10-22 13:05:35 +0530

    Learning C#

--- FirstProject/Assets/Scripts/firstScript.cs
+++ FirstProject/Assets/Scripts/firstScript.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class firstScript : MonoBehaviour
 {
     // Start is called before the first frame update
+    public string name123="name";
     void Start()
     {
         print("Hello everyone");
@@ -16,6 +17,11 @@ public class firstScript : MonoBehaviour
         print(100.474f);
         print(100.474f*10.33f);
         print("google".Substring(3,3));
+        print(name123);
+        bool st=1==1;
+        bool st1=1!=1;
+        print(st);
+        print(st1);
     }
 
     // Update is called once per frame
